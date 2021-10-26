75d1b6922342999ac2df9eea0150ab1096bf4023
Srikanth <srikanthh994@gmail.com>
2021-10-26 10:31:03 +0530

    Added:Added Last NameValidation

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,12 +1,16 @@
 package com.bl.userregistration;
 
 public class UserRegistrationMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Registration System");
-		// Creating Variable With Using Of UserRegistration Class
-		UserRegistration userRegistration = new UserRegistration();
-		// Here Calling FirstName Method Storing The Result In Result
-		boolean firstName = userRegistration.FirstNameValidation("Srikanth");
-		System.out.println(firstName);// Printing Result
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Registration System");
+        //Creating Variable With Using Of UserRegistration Class
+        UserRegistration userRegistration = new UserRegistration();
+        //Here Calling FirstName Method Storing The Result In Result
+        boolean firstName = userRegistration.FirstNameValidation("Srikanth");
+        System.out.println(firstName);//Printing Result
+        //Here Calling Validate FirstName Method Storing The Result In Result
+        boolean lastName = userRegistration.FirstNameValidation("Housenoor");
+        System.out.println(lastName);//Printing Result
+    }
+
 }
